4949ab0d845615f361a5f14e9a1dfa7f8080f00b
yxl040840219 <yxl040840219@126.com>
2017-04-26 15:15:42 +0800

    update

--- export/hivetype.py
+++ export/hivetype.py
@@ -10,7 +10,7 @@ class HiveType:
         if ctype in ("varchar", "char"):
             ctype = "string"
         if ctype in ("datetime",):
-            ctype = "timestamp"
+            ctype = "string"
         if ctype == "timestamp":
             ctype = "string"
         if ctype in ("text", "longtext"):
