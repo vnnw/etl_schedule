d7b6f6aac48c2309be037b4f415388f88280a046
yxl040840219 <yxl040840219@126.com>
2017-04-27 11:10:22 +0800

    update

--- sql/ods_driver_remind.sql
+++
@@ -1,7 +0,0 @@
-create external table if not exists ods.ods_driver_remind (
-    id bigint comment "竞价司机岗前提醒 ID" ,
-    bid_id bigint comment "司机竞价 ID" ,
-    source int comment "调用者来源" ,
-    operator_id bigint comment "操作人 ID" ,
-    created_at string comment "创建时间" )
- stored as orc
--- sql/ods_trans_event_finance_pay_type.sql
+++
@@ -1,4 +0,0 @@
-create external table if not exists ods.ods_trans_event_finance_pay_type (
-    trans_event_id bigint comment "运力id" ,
-    finance_pay_type int comment "客户类型（客户的财务支付类型）" )
- stored as orc
