aac8a0e498ecf71295686d92dc09ec1ea20f7551
yxl040840219 <yxl040840219@126.com>
2017-04-28 17:24:27 +0800

    update

--- sql/ods_trans_event_snapshot.sql
+++
@@ -1,5 +0,0 @@
-create external table if not exists ods.ods_trans_event_snapshot (
-    `trans_event_id`  bigint comment "运力id" ,
-    `project_type`  int comment "客户项目类型" ,
-    `bu_leader_id`  bigint comment "客户所属bu leader的id" )
- stored as orc
