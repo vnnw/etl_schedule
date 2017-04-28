021b1ea6bab80b96fa1b468c5fd64ee0b197fc8b
root <root@yn-vm-test-202-72.xhj.com>
2017-04-28 16:14:25 +0800

    update

--- sql/ods_trans_task_visit.sql
+++ sql/ods_trans_task_visit.sql
@@ -1,6 +1,6 @@
 create external table if not exists ods.ods_trans_task_visit (
-    id bigint comment "visit id" ,
-    trans_task_id bigint comment "任务id" ,
-    driver_id bigint comment "司机id" ,
-    created_at timestamp comment "创建时间" )
+    `id`  bigint comment "visit id" ,
+    `trans_task_id`  bigint comment "任务id" ,
+    `driver_id`  bigint comment "司机id" ,
+    `created_at`  timestamp comment "创建时间" )
  stored as orc
