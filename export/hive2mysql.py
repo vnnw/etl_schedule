f5f41ed65aab481641e36b03ebde578cd57eb67b
yxl040840219 <yxl040840219@126.com>
2017-05-26 10:03:51 +0800

    update

--- export/hive2mysql.py
+++ export/hive2mysql.py
@@ -53,7 +53,7 @@ def hive_connection(db):
 def create_tmp_dir():
     mills = datetime.datetime.now().microsecond
     rand = random.randint(1, 100)
-    tmpdatadir = config_util.get("tmp.path") + "/hdatas" + str(rand) + "/" + str(mills)
+    tmpdatadir = config_util.get("tmp.path") + "/hdatas/" + str(rand) + "/" + str(mills)
     if not os.path.exists(tmpdatadir):
         os.makedirs(tmpdatadir)
     return tmpdatadir
