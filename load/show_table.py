a888a43aa19ec53f8923b4ae37d20de858fd34af
yxl040840219 <yxl040840219@126.com>
2017-06-28 13:48:08 +0800

    update

--- load/show_table.py
+++ load/show_table.py
@@ -24,14 +24,25 @@ def get_tables(connection, db):
     return tables
 
 
+def get_hive_tables(connection):
+    cursor = connection.cursor()
+    cursor.execute("show tables")
+    result = cursor.fetchall()
+    tables = set()
+    for table in result:
+        tables.add(table[0])
+    return tables
+
+
 def get_option_parser():
     usage = "usage: %prog [options] arg1 arg2"
 
     parser = OptionParser(usage=usage)
 
-    parser.add_option("-t", "--table", dest="table", action="store", help="database table")
+    parser.add_option("-t", "--table", dest="table", action="store", help="mysql table name")
 
     return parser
+
 
 if __name__ == '__main__':
     reload(sys)
@@ -41,6 +52,10 @@ if __name__ == '__main__':
 
     options, args = optParser.parse_args(sys.argv[1:])
 
+    if options.table is None:
+        optParser.print_help()
+        sys.exit(1)
+
     table = options.table.strip()
 
     db_set = set()
@@ -48,14 +63,22 @@ if __name__ == '__main__':
         if key.startswith("mysql") and len(key.split(".")) == 3:
             mysql, db, config = key.split(".")
             db_set.add(db)
+    hive_connection = Connection.get_hive_connection(config_util, "ods_mysql")
     for db in db_set:
+        hive_table = "ods_" + db + "__" + table
+        hive_tables = get_hive_tables(hive_connection)
+        if hive_table.strip() in hive_tables:
+            print table + " 在hive中已经存在: ods_mysql." + hive_table
+            break
         connection = Connection.get_mysql_connection(config_util, db)
         tables = get_tables(connection, db)
+        connection.close()
         if table.strip() in tables:
             print "database: " + db + " table:" + table
             config = Connection.get_mysql_config(config_util, db)
             sql = "mysql -u" + config.get("username") + " -p" + \
                   config.get("password") + " -h" + config.get("host") + \
-                  " -P" + config.get("port")
+                  " -P" + config.get("port") + " -D" + db
             print sql
             break
+    hive_connection.close()
